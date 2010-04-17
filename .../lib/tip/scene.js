30a5e5c40fc4cc110ec2bda99251450118996c5b
Will <will@fenrir.himinbi.org>
2010-04-17 00:48:50 -0400

    checkpoint

--- .../lib/tip/scene.js
+++ .../lib/tip/scene.js
@@ -2,6 +2,7 @@ var scene = new List()
 ;
 ( function() {
     function once() {
+        var state = new List()
         // ToDo: capture metrics about execution times
         //        and attempt to learn impression.display.time
         //        which is the time that the first 'display'
@@ -12,17 +13,24 @@ var scene = new List()
             'time.impression.display.predicted' : ( new Date() ).getTime() + 20,
             'state.impression' : 'configuration',
         } ) )
-        var count = 0
-        do {
-            state.__.set( new List( {
-                'impression.configuration.done' : true,
-                'impression.loop.count' : ++count,
-            } ) )
-            //console.log( 'Starting: once: config:' + state.__.get('impression.configuration.done') )
-            this.__.impress.apply( state )
-            //console.log( 'Finished: once: config:' + state.__.get('impression.configuration.done') )
-        } while( count < 100
-                 && ! state[ 'impression.configuration.done' ] )
+
+        function config( ) {
+            var $this = $(this)
+            state = state || {}
+            if( state.__ && typeof state.__.set == 'function' ) {
+                state.__.set( 'caller', this )
+            }
+            var cfg = $this.data( 'config' )
+            if( typeof cfg == 'function'
+                && cfg != undefined ) {
+                cfg.apply( state, arguments )
+            }
+        }
+        $.__.$('#').each( function traverse( ) {
+            config.apply( this, arguments )
+            $(this).children().each( traverse ) 
+        } )
+
         state.__.set( {
             'impression.display.time' : ( new Date() ).getTime(),
             'impression.configuration.time' : (
@@ -31,47 +39,19 @@ var scene = new List()
             ),
             'impression.state' : 'display',
         } )
-        do {
-            state.__.set( new List( {
-                'impression.display.done' : true,
-                'impression.loop.count' : ++count,
-            } ) )
-            this.__.impress.apply( state )
-            //console.log( 'Finished: once: display:' + state.__.get('impression.display.done') )
-        } while( ++count < 100
-                 && ! state[ 'impression.display.done' ] )
-        
-        var tixels = state.__.get( 'tixels' )
-        if( ! tixels )
-            throw 'no tixels'
-        var time = state.__.get( 'impression.display.time' )
-        
-        tixels.__.each( function( txl ) {
-            if( time > txl.time.start && time <= txl.time.end
-                || time >= txl.time.end && ! txl.done ) {
-                txl.time.delta = ( txl.time.delta
-                                             || txl.time.end - txl.time.start )
-                var offset = ( time >= txl.time.end
-                               ? 1
-                               : ( ( time - txl.time.start )
-                                   / txl.time.delta ) )
-                txl.val.delta = ( txl.val.delta
-                                  || txl.val.end - txl.val.start )
-                var val = txl.val.start + offset * txl.val.delta
-                if( txl.prop.each ) {
-                    txl.prop.each( function( prop ) {
-                                  txl.obj[ prop ] = val
-                    } )
-                } else {
-                    txl.obj[ txl.prop ] = val
-                }
-                
-                txl.obj[ txl.prop ] = 
-                    txl.val.start + offset * txl.val.delta
-                txl.done = offset === 1
+
+        function display( ) {
+            var $this = $(this)
+            var scr = $this.data( 'display' )
+            if( typeof scr == 'function'
+                && scr != undefined ) {
+                scr.apply( this, arguments )
             }
+        }
+        $.__.$('#').each( function traverse( ) {
+            display.apply( this, arguments )
+            $(this).children().each( traverse ) 
         } )
-        
         return state
     }
     
