424410c25ec2a0ba4e339a53832fb01b9abba30e
Will <will@loki.himinbi.org>
2010-04-19 11:14:22 -0400

    checkpoint

--- .../lib/tip/scene.js
+++ .../lib/tip/scene.js
@@ -20,10 +20,9 @@ var scene = new List()
         //        which is the time that the first 'display'
         //        event will pass through
         var time = {
-            offset : ( ( new Date() ).getTime() - state.__.get( 'time.epoch' ) ) * 2
-                // state.__.get( 'tixel.to.scene.time.ratio' ) ),
+            offset : ( ( ( new Date() ).getTime() - state.__.get( 'time.epoch' ) )
+                       * state.__.get( 'tixel.to.scene.time.ratio' ) ),
         }
-        console.log( time.offset )
 
         function config( ) {
             var $this = $(this)
