473149df56ccc67717081eb04bbc084f504c748d
Will Holcomb <will@dhappy.org>
2010-05-04 16:49:49 -0400

    checkpoint

--- .../lib/tip/test/EventSource server.php
+++ .../lib/tip/test/EventSource server.php
@@ -1,9 +1,6 @@
 <?php
 header("Content-Type:text/event-stream");
 
-echo "event: server-time\n";
 $time = date( 'Y/m/d @ H:i:s.u' );
+echo "event: server-time\n";
 echo "data: $time\n\n";
-
-echo "event: count\n";
-sprintf( "data: %s \n\n", rand() );
