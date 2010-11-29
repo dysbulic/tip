3679c727dbbfb0895d110c9078dba305bba50afd
Will Holcomb <will@dhappy.org>
2010-11-28 21:30:32 -0800

    working on tab content position

--- index.js
+++ index.js
@@ -1,95 +1,112 @@
 $(function() {
+  var setup = function() {
+    var $body = $('body');
+    $body.addClass('tabbed')
+
+    $('head').append( $('<link />').attr( {
+      rel : "stylesheet",
+      href : "tabbed.css",
+      type : "text/css",
+    } ) );
+
+    /** Add top level HTML/jQuery tabs **/
+
+    // Begin by collecting the current body into a single root
+    var $origroot = $('<div/>');
+    $body.children().appendTo( $origroot );
+    $body.append( $origroot );
   
-  var $body = $('body');
-  $body.addClass('tabbed')
+    var $top = $("<div id='top'><ul></ul></div>");
+    $body.prepend( $top );
 
-  $('head').append( $('<link />').attr( {
-    rel : "stylesheet",
-    href : "tabbed.css",
-    type : "text/css",
-  } ) );
-
-  /** Add top level HTML/jQuery tabs **/
-
-  // Begin by collecting the current body into a single root
-  var $origroot = $('<div/>');
-  $body.children().appendTo( $origroot );
-  $body.append( $origroot );
-  
-  var $top = $("<div id='top'><ul></ul></div>");
-  $body.prepend( $top );
-
-  var $toptabs = $top.tabs( {
-    tabTemplate: "<li><a href='#{href}'>#{label}</a></li>",
-  } );
-
-  // Callback to move the current body elements to the contents of a tab
-  var moveBody = function( event, ui ) {
-    $origroot.fadeOut( 'fast', function() {
-      $(ui.panel).append( $origroot );
-      $origroot.fadeIn( 'fast' );
+    var $toptabs = $top.tabs( {
+      tabTemplate: "<li><a href='#{href}'>#{label}</a></li>",
     } );
-  };
-  $toptabs.bind( "tabsadd", moveBody );
-  $toptabs.tabs("add", "#htmltab", "<acronym title='Hypertext Markup Language'>HTML</acronym>");
-  $toptabs.unbind( "tabsadd", moveBody );
 
-  var $root;
-  $toptabs.tabs( {
-    add: function( event, ui ) {
-      $root = $(ui.panel);
-    }
-  } );
-  $toptabs.tabs("add", "#jquerytab", "jQuery <acronym title='User Interface'>UI</acronym>");
-  
-  var $areas = $("<div id='areas'><ul></ul></div>");
-  $root.append( $areas );
-  var $topictabs = $areas.tabs( {
-    tabTemplate: "<li><a href='#{href}'>#{label}</a></li>",
-  } );
-
-  //jQuery.getJSON( 'resume.json', function(data, textStatus, xhr) {
-    // Runs on load to populate the initial tabs
-  var tabcount = 0;
-  $origroot.find('#toplist').children().each(function( idx, elem ) {
-    var $src = $(elem);
-    if( $src.children().hasClass( 'title' ) ) {
-      function newTab() {
-        $topictabs.tabs( {
-          add: function( event, ui ) {
-            buildTimeline( $src, $(ui.panel) );
-          }
-        } );
-        var title = $src.children( '.title' ).text();
-        $topictabs.tabs("add", "#tabs-" + ++tabcount, title);
-      }
-      setTimeout(newTab, 15 * idx);
-    }
-  } );
-
-  function buildTimeline( $src, $root ) {
-    //$src.children().clone().appendTo( $root );
-    var $headlist = $('<ul id="headers"/>');
-    var $proplist = $('<ul id="props"/>');
-    $src.children().children( 'li' ).each( function( idx, elem ) {
-      var $pitem = $('<li/>');
-      $pitem.append( $(elem).children( 'ul' ).clone() );
-      $proplist.append( $pitem );
-
-      var $hitem = $('<li/>');
-      $hitem.append( $(elem).children( '.org' ).clone() );
-      $hitem.append( $(elem).children( '.role' ).clone() );
-      $hitem.click( function() {
-	  var offset = $pitem.offset().top;
-	  $('#props').animate( {
-	    scrollTop: offset
-	  }, 1000);
-	  console.log(offset);
+    // Callback to move the current body elements to the contents of a tab
+    var moveBody = function( event, ui ) {
+      $origroot.fadeOut( 'fast', function() {
+        $(ui.panel).append( $origroot );
+        $origroot.fadeIn( 'fast' );
       } );
-      
-      $headlist.append( $hitem );
+    };
+    $toptabs.bind( "tabsadd", moveBody );
+    $toptabs.tabs("add", "#htmltab", "<acronym title='Hypertext Markup Language'>HTML</acronym>");
+    $toptabs.unbind( "tabsadd", moveBody );
+
+    var $root;
+    $toptabs.tabs( {
+      add: function( event, ui ) {
+        $root = $(ui.panel);
+      }
     } );
-    $root.append( $headlist );
-    $root.append( $proplist );
+    $toptabs.tabs("add", "#jquerytab", "jQuery <acronym title='User Interface'>UI</acronym>");
+  
+    var $areas = $("<div id='areas'><ul></ul></div>");
+    $root.append( $areas );
+    var $topictabs = $areas.tabs( {
+      tabTemplate: "<li><a href='#{href}'>#{label}</a></li>",
+    } );
+
+    //jQuery.getJSON( 'resume.json', function(data, textStatus, xhr) {
+    // Runs on load to populate the initial tabs
+    var tabcount = 0;
+    $origroot.find('#toplist').children().each(function( idx, elem ) {
+      var $src = $(elem);
+      if( $src.children().hasClass( 'title' ) ) {
+        function newTab() {
+          $topictabs.tabs( {
+            add: function( event, ui ) {
+              buildTimeline( $src, $(ui.panel) );
+            }
+          } );
+          var title = $src.children( '.title' ).text();
+          $topictabs.tabs("add", "#tabs-" + ++tabcount, title);
+        }
+        setTimeout(newTab, 15 * idx);
+      }
+    } );
+
+    console.log( $top.children( '.ui-tabs-nav' ).height() );
+    $('#jquerytab').height( $(window).height() - $top.children( '.ui-tabs-nav' ).height() );
+
+    function buildTimeline( $src, $root ) {
+      //$src.children().clone().appendTo( $root );
+      var $headlist = $('<ul id="headers"/>');
+      var $proplist = $('<ul id="props"/>');
+      $src.children().children( 'li' ).each( function( idx, elem ) {
+        var $pitem = $('<li/>');
+        $pitem.append( $(elem).children( 'ul' ).clone() );
+        $proplist.append( $pitem );
+        
+        var $hitem = $('<li/>');
+        $hitem.append( $(elem).children( '.org' ).clone() );
+        $hitem.append( $(elem).children( '.role' ).clone() );
+        $hitem.click( function() {
+	  var offset = $pitem.offset().top;
+	  $('#props').animate(
+            {
+	      scrollTop: offset
+            },
+            1000 );
+	  console.log(offset);
+        } );
+      
+        $headlist.append( $hitem );
+      } );
+      $root.append( $headlist );
+      $root.append( $proplist );
+    }
+  }
+  /* Load content via ajax if not inserted via php */
+  if( $('body').children().size() == 1 ) {
+    $.get( 'resume.html', function( data ) {
+      $('body').append( $('<div/>' ).html( data ) );
+      setup();
+    } )
+  } else {
+    console.log($('body').children().size());
+    setup();
   }
 } );
+  
