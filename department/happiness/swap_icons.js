0582655d2f0a46a68fc4bf961a90154fafa0a490
Will Holcomb <will@dhappy.org>
2011-01-04 21:11:08 -0500

    working on click errors with logo

--- department/happiness/swap_icons.js
+++ department/happiness/swap_icons.js
@@ -43,56 +43,99 @@ $( function() {
     function scroller() {
     } 
 
-    var $root = $(document.documentElement);
-    $root.find( '[class="menuitem"]' ).each( function() {
-        var $item = $(this);
-        $item.click( function() {
-            var $item = $(this);
-            var showsub = ! $item.data( 'showsub' );
-            $item.data( 'showsub', showsub )
-
-            $item.parents().andSelf().each( function() {
-                var $parent = $(this);
+    var $selected
+    function toggleItem( $item, showsub ) {
+        if( $item == undefined ) {
+            return
+        }
+        console.log( 'toggle:' + showsub )
+        $item.data( 'showsub', showsub )
+        $item.parents().andSelf().each( function() {
+            var $parent = $(this);
                 
-                if( showsub ) {
-                    var transform = $parent.attr( 'transform' )
-                    // if no transform is set, use the identity transform
-                    if( transform == undefined || transform == '' ) {
-                        transform = 'matrix(1,0,0,1,0,0)'
-                        $parent.attr( 'transform', transform )
-                    }
-                    $parent.data( 'transform', transform )
-                    $parent.animate( {
-                        transform : 'matrix(1,0,0,1,0,0)',
-                    } );
-                } else {
-                    $parent.animate( {
-                        transform : $parent.data( 'transform' ),
-                    } );
-                }
-            } );
-
             if( showsub ) {
-                $root.data( 'viewBox', $root.attr( 'viewBox' ) );
-                $root.animate( {
-                    //viewBox : '0 0 ' + $item.width() + ' ' + $item.height(),
-                    svgViewBox : '0 0 100 100', // ToDo: dynamically determine width and height
-                } )
-            } else {
-                $root.animate( {
-                    svgViewBox : $root.data( 'viewBox' ),
-                } )
-            }
-
-            $item.parent().children().each( function() {
-                var $child = $(this)
-                if( !$child.equals( $item ) ) {
-                    $child.animate( {
-                        opacity : showsub ? '1' : '0',
-                    } )
+                var transform = $parent.attr( 'transform' )
+                // if no transform is set, use the identity transform
+                if( transform == undefined || transform == '' ) {
+                    transform = 'matrix(1,0,0,1,0,0)'
+                    $parent.attr( 'transform', transform )
                 }
-            } );
+                $parent.data( 'transform', transform )
+                $parent.animate( {
+                    transform : 'matrix(1,0,0,1,0,0)',
+                } );
+            } else {
+                $parent.animate( {
+                    transform : $parent.data( 'transform' ),
+                } );
+            }
         } );
+
+        if( showsub ) {
+            $root.data( 'viewBox', $root.attr( 'viewBox' ) )
+            $root.animate( {
+                //viewBox : '0 0 ' + $item.width() + ' ' + $item.height(),
+                svgViewBox : '0 0 100 100', // ToDo: dynamically determine width and height
+            } )
+        } else {
+            $root.animate( {
+                svgViewBox : $root.data( 'viewBox' ),
+            } )
+        }
+        
+        // Search up the tree for a level that is branched
+        var $branched = $item
+        while( $branched.parent() && $branched.parent().children().size() == 1 ) {
+            $branched = $branched.parent()
+        }
+        $branched.parent().children().not( $branched ).each( function() {
+            var $child = $(this)
+            if( showsub ) {
+                $child.data( 'transform', $child.attr( 'transform' ) )
+            }
+            var transform = showsub ? 'matrix(0,0,0,0,75,75)' : $child.data( 'transform' )
+            //tip.get( 'debug' ) &&
+            //console.log( showsub + ':' + transform )
+            $child.css( 'display', '' )
+            $child.animate( {
+                  opacity : showsub ? '0' : '1',
+                  transform : transform,
+              },
+              function() {
+                  // Click areas for links are not being removed
+                  $(this).css( 'display', showsub ? 'none' : '' )
+              } )
+        } )
+
+        // Save for use when reversing animation
+        $selected = showsub ? $item : undefined
+    }
+
+    var $root = $(document.documentElement)
+    $root.find( 'a' ).each( function() {
+        var $item = $(this)
+        $item.click( function() {
+            var $clicked = $(this)
+            var id = $clicked.attr( 'id' )
+            // If the selected item is clicked when selected, unselect
+            if( $selected && $selected.attr( 'id' ) == id ) {
+                id = ''
+            }
+            $.history.load( id )
+
+            return false
+        } );
+    } )
+
+    var title = $root.children( 'title' ).text()
+    $.history.init( function( hash ) {
+        console.log(hash)
+        if( hash == '' ) {
+            toggleItem( $selected, false )
+        } else {
+            toggleItem( $('#' + hash), true )
+        }
+        $root.children( 'title' ).text( title + ': ' + hash )
     } )
 } )
 
