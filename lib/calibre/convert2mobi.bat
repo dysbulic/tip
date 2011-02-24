6aa9a550c9da10478321a8c92d33787b040f1188
will <will@dhappy.org>
2011-02-24 14:53:12 -0500

    added something I forgot

--- lib/calibre/convert2mobi.bat
+++ lib/calibre/convert2mobi.bat
@@ -1,9 +1,13 @@
-@if [%1]==[] goto :usage
-@if %1=="" goto :usage
+if [%1]==[] goto :usage
+if %1=="" goto :usage
 
 @set input=%1
 @set output="%~dp1%~n1.mobi"
+
+@echo Converting: %input%
+
 @set args=--page-breaks-before="/"
+@set args=%args% --chapter="//h:section/*[name()='h1' or name()='h2' or name()='h3']"
 
 @set converter="%ProgramFiles%\Calibre2\ebook-convert.exe"
 @if exist %converter% goto :convert
@@ -16,11 +20,15 @@
 
 :convert
 
+@set searchscript="%~dp0find_cover.js"
 @set cover=
-@for /f "tokens=*" %%l in ('cscript /nologo "%~dp0find_cover.js" %input%') do @set cover=%%l
+@if not exist %searchscript% goto :postcover
+@for /f "tokens=*" %%l in ('cscript /nologo %searchscript% %input%') do @set cover=%%l
 @if not [%cover%]==[] @set args=%args% --cover="%~dp1%cover%"
 
-%converter% %input% %output% %args%
+:postcover
+
+%converter% %input% %output% %args% | tee convert2mobi.out.log
 goto :end
 
 :usage
@@ -28,5 +36,5 @@ goto :end
 @echo Usage: convert2mobi [input]
 
 :end
-rem pause
-rem set /p press_return
+@rem pause
+@rem set /p press_return
