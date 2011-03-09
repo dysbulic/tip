0150c45d7a8bee3def3cd8b456fc755fb2835485
will <will@dhappy.org>
2011-03-08 23:30:50 -0500

    basic download running

--- lib/calibre/download_bot.py
+++ lib/calibre/download_bot.py
@@ -1,8 +1,6 @@
-#! /usr/bin/env python
+#! /usr/bin/python
 #
-# Example program using ircbot.py.
-#
-# Joel Rosdahl <joel@rosdahl.net>
+# Program to download award winners from irc channel
 
 """A simple example bot.
 
@@ -28,42 +26,163 @@ The known commands are:
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad, ip_quad_to_numstr
 
-import os
-import re
+import os, re, glob
 import struct
 import logging
 import zipfile
 from zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
 from contextlib import closing
+import random
 
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-class TestBot(SingleServerIRCBot):
+class Search:
+    def __init__(self, author = "", title = ""):
+        self.author = author
+        self.title = title
+        self.results = []
+        self.matches = None
+        self.active_source = None
+        
+    @property
+    def phrase(self):
+        last_name = self.author.split()[-1] if len(self.author) > 0 else ""
+        return "%s%s%s" % (last_name, " " if len(last_name) > 0 else "", self.title)
+        
+
+class UnknownPhrase(Exception):
+    pass
+
+class SearchManager():
+    def __init__(self):
+        self.use_count = {}
+        self.searches = {}
+
+    def add(self, search):
+        if self.searches.has_key(search.phrase):
+            log.error("Duplicate Search: " + search.phrase)
+        self.link(search.phrase, search)
+        return search
+
+    def get(self, phrase):
+        if self.searches.has_key(phrase):
+            return self.searches[phrase]
+        else:
+            raise UnknownPhrase(phrase)
+
+    def link(self, key, search):
+        self.searches[key] = search
+
+    def get_source(self, search):
+        """Return the result whose server is least used"""
+        min_result = None
+        for result in search.results:
+            if not self.use_count.has_key(result.server):
+                self.use_count[result.server] = 0
+            rcnt = self.use_count[min_result.server] if min_result is not None else None # Result Count
+            ccnt = self.use_count[result.server]     # Current Count
+
+            # Find minimum, coin toss for ties
+            if(not result.attempted
+               and result.is_html
+               and (min_result is None
+                    or rcnt < ccnt
+                    or (rcnt == ccnt and random.random() > .5))):
+               min_result = result
+        if min_result is not None:
+            self.use_count[min_result.server] = self.use_count[min_result.server] + 1
+        return min_result
+
+class DownloadSource():
+    def __init__(self, server, filename, size):
+        self.server = server
+        self.filename = filename
+        self.size = size
+        self.attempted = False
+        
+    @property
+    def req(self):
+        """IRC request command"""
+        return "!%s %s" % (self.server, self.filename)
+
+    @property
+    def is_html(self):
+        return re.search("(\[[^\]]*htm[^\]]*\]|\([^\)]*htm[^\)]*\)|\.htm)", self.filename) is not None
+
+searches = SearchManager()
+search_queue = []
+
+class DownloadBot(SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667):
         SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
 
-    def on_nicknameinuse(self, c, e):
-        newnick = c.get_nickname() + "_"
+    def on_nicknameinuse(self, connection, event):
+        newnick = connection.get_nickname() + "_"
         log.info("Nick Collision: " + newnick)
-        c.nick(newnick)
+        connection.nick(newnick)
 
-    def on_welcome(self, c, e):
+    def on_welcome(self, connection, event):
         log.info("Joining: " + self.channel)
-        c.join(self.channel)
+        connection.join(self.channel)
 
-    def on_privmsg(self, c, e):
-        log.info("Privmsg: " + e.arguments()[0])
-        self.do_command(e, e.arguments()[0])
+    def on_privmsg(self, connection, event):
+        log.info("Privmsg: " + event.arguments()[0])
+        self.do_command(event, event.arguments()[0])
 
-    def on_pubmsg(self, c, e):
-        log.info("Pubmsg: " + e.arguments()[0])
-        a = e.arguments()[0].split(":", 1)
-        if len(a) > 1 and irc_lower(a[0]) == irc_lower(self.connection.get_nickname()):
-            self.do_command(e, a[1].strip())
+    def on_privnotice(self, connection, event):
+        msg = event.arguments()[0]
+        stripformat = re.compile("(\x16|\x0f|\x1f|\x02|\x03(\d{1,2}(,\d{1,2})?)?)", re.UNICODE)
+        msg = stripformat.sub("", msg)
+        
+        accept = re.match(r"<<SearchBot>> Your search for \"(.*)\" has been accepted.", msg)
+        if accept is not None:
+            search_str = accept.group(1)
+            try:
+                log.info("Search accepted for '%s'" % search_str)
+                searches.get(search_str)
+            except UnknownPhrase:
+                log.error("Search for '%s' not recorded" % search_str)
+                searches.add(Search(title=search_str))
+            return
+
+        missing = re.match("<<SearchBot>> Sorry, your search for \"(.*)\" returned no matches.", msg)
+        if missing is not None:
+            search_str = missing.group(1)
+            searches.get(search_str).matches = 0
+            log.info("No results for '%s'" % search_str)
+            self.queue_next()
+            return
+
+        found = re.match("<<SearchBot>> Your search for \"(.*)\" returned (\d+) match", msg)
+        if found is not None:
+            search_str = found.group(1)
+            count = int(found.group(2))
+            searches.get(search_str).matches = count
+            log.info("%d results for %s" % (count, search_str))
+            return
+
+        welcome = re.match("\[#ebooks\] Welcome to #ebooks", msg)
+        if welcome is not None:
+            self.queue_next()
+            return
+
+        log.info("PrivNotice: " + msg)
+        
+    def on_pubnotice(self, connection, event):
+        log.info("PubNotice: " + event.arguments()[0])
+
+    def on_pubmsg(self, connection, event):
+        #log.info("Pubmsg: " + event.arguments()[0])
+        args = event.arguments()[0].split(":", 1)
+        if len(args) > 1 and irc_lower(args[0]) == irc_lower(self.connection.get_nickname()):
+            self.do_command(event, args[1].strip())
         return
+
+    def on_ctcpreply(self, connection, event):
+        log.info("CTCP Reply: " + event.arguments()[0])
 
     def on_ctcp(self, connection, event):
         log.info("CTCP Arg[0]: " + event.arguments()[0])
@@ -79,10 +198,19 @@ class TestBot(SingleServerIRCBot):
         dcc.filename = os.path.basename(args.group('filename'))
 
         search = re.match("^SearchBot_results_for_(?P<name>.*).txt.zip", dcc.filename)
-        if search is not None:
-            dcc.is_search = True
-            dcc.search_str = search.group('name')
-            log.info("Search Result: " + dcc.search_str)
+        dcc.is_search = search is not None
+        if dcc.is_search:
+            search_str = search.group('name')
+            try:
+                dcc.search = searches.get(search_str)
+                log.info("Search Result: " + search_str)
+            except UnknownPhrase:
+                dcc.search = searches.add(Search(title=search_str))
+                log.error("Unknown Search: " + search_str)
+        else:
+            dcc.search = searches.get(dcc.filename)
+
+        dcc.search.filename = dcc.filename
 
         if os.path.exists(dcc.filename):
             count = 1
@@ -104,16 +232,16 @@ class TestBot(SingleServerIRCBot):
         log.info("Received file %s (%d bytes)." % (connection.filename, connection.received_bytes))
         connection.disconnect()
         if connection.is_search:
-            self.from_search(connection.filename)
+            self.process_search(connection.search)
+        else:
+            self.name_download(connection.search)
+        self.queue_next()
                  
-    #def on_dccmsg(self, c, e):
-    #    c.privmsg("You said: " + e.arguments()[0])
-
-    def on_dccchat(self, c, e):
-        log.info("DCC Chat: " + e.arguments()[0])
-        if len(e.arguments()) != 2:
+    def on_dccchat(self, connection, event):
+        log.info("DCC Chat: " + event.arguments()[0])
+        if len(event.arguments()) != 2:
             return
-        args = e.arguments()[1].split()
+        args = event.arguments()[1].split()
         if len(args) == 4:
             try:
                 address = ip_numstr_to_quad(args[2])
@@ -122,9 +250,16 @@ class TestBot(SingleServerIRCBot):
                 return
             self.dcc_connect(address, port)
 
-    def do_command(self, e, cmd):
-        nick = nm_to_n(e.source())
+    def do_command(self, event, cmd):
+        nick = nm_to_n(event.source())
         c = self.connection
+
+        # cmd, arg = cmd.split(r" ", 1)
+        splitcmd = cmd.split(r" ", 1)
+        arg = None
+        if len(splitcmd) > 1:
+            cmd = splitcmd[0]
+            arg = splitcmd[1]
 
         if cmd == "disconnect":
             self.disconnect()
@@ -144,8 +279,10 @@ class TestBot(SingleServerIRCBot):
                 voiced.sort()
                 c.notice(nick, "Voiced: " + ", ".join(voiced))
         elif cmd == "get":
-            #self.search_for("The Lifecycle of Software Objects")
-            self.search_for("Echo Jack McDevitt")
+            if arg is None:
+                c.notice(nick, "get expects an argument")
+            else:
+                self.search_for(arg)
         elif cmd == "dcc":
             dcc = self.dcc_listen()
             c.ctcp("DCC", nick, "CHAT chat %s %d" % (
@@ -154,36 +291,98 @@ class TestBot(SingleServerIRCBot):
         else:
             c.notice(nick, "Not understood: " + cmd)
 
-    def search_for(self, search_str):
-        c = self.connection
-        cmd = "@search %s" % search_str
-        chname, chobj = self.channels.items()[0]
-        log.info("Sending: '%s' to %s" % (cmd, chname))
-        c.privmsg(chname, cmd)
+    def search_for(self, search):
+        searches.add(search)
+        
+        previous = "SearchBot_results_for_\"%s\".txt.zip" % search.phrase
+        if os.path.exists(previous):
+            log.info("Using cached search: " + previous)
+            search.filename = previous
+            process_search(search)
+        else:
+            c = self.connection
+            cmd = "@search %s" % search.phrase
+            chname, chobj = self.channels.items()[0]
+            log.info("Sending: '%s' to %s" % (cmd, chname))
+            c.privmsg(chname, cmd)
 
-    def from_search(self, filename):
-        with closing(ZipFile(open(filename, 'rb'))) as inf:
+    def process_search(self, search):
+        with closing(ZipFile(open(search.filename, 'rb'))) as inf:
             name = inf.namelist()[0]
             log.info("Zipped: " + name)
             lines = inf.read(name).split("\n")[4:] # Trim header
             results = []
             for line in lines:
-                result = line.split("  ")[0]
-                match = re.search(r"^!(\S+).*(\[[^\]]*htm[^\]]*\]|\([^\)]*htm[^\)]*\)|\.htm)", result)
-                if match is not None:
-                    results.append(result)
-                    server = match.group(1)
+                parts = re.match(r"^!(?P<server>\S+)\s+(?P<file>.*?)  (.*)\s+(?P<size>\d+(.\d+)?)\s*(?P<unit>.?[Bb])", line)
+                if parts is None:
+                    log.error("Unknown Result Match: " + line)
+                else:
+                    src = DownloadSource(parts.group('server'),
+                                                  parts.group('file'),
+                                                  parts.group('size') + parts.group('unit'))
+                    results.append(src)
 
-        c = self.connection
-        chname, chobj = self.channels.items()[0]
-        log.info("Sending: '%s' to %s" % (results[0], chname))
-        c.privmsg(chname, results[0])
+        search.results = results
+
+        source = searches.get_source(search)
+        if source is None:
+            log.error("No results for: " + search.phrase)
+        else:
+            searches.link(source.filename, search)
+            c = self.connection
+            chname, chobj = self.channels.items()[0]
+            log.info("Sending: '%s' to %s" % (source.req, chname))
+            c.privmsg(chname, source.req)
+            source.attempted = True
+
+    def name_download(self, search):
+        filename = "%s - %s.html.rar" % (search.author, search.title)
+        if filename != search.filename:
+            log.info("Renaming %s to %s" % (search.filename, filename))
+            os.rename(search.filename, filename)
+
+    def queue_next(self):
+        if len(search_queue) > 0:
+            self.search_for(search_queue.pop())
+
+
+import freebase
+
+query = {
+    "id" :   "/en/nebula_award",
+    "type" : "/award/award",
+    "category" : [{
+      "name" : None,
+      "nominees" : [{
+        "award_nominee" : [{
+          "name" : None,
+        }],
+        "nominated_for" : [{
+          "name" : None,
+        }],
+        "limit" : 5000,
+      }]
+    }]
+}
+
+results = freebase.mqlread(query)
+
+for category in results.category:
+    for nominee in category.nominees:
+        author = nominee.award_nominee[0].name
+        title = nominee.nominated_for[0].name
+
+        dbpath = glob.glob("%s*%s*" % (author, title))
+        if len(dbpath) == 0:
+            search_queue.append(Search(author, title))
+        else:
+            log.info("Found Path: " + dbpath[0])
 
 def main():
     import sys
 
     if len(sys.argv) != 4:
-        print "Usage: testbot <server[:port]> <channel> <nickname>"
+        log.info("Usage: testbot <server[:port]> <channel> <nickname>")
         sys.exit(1)
 
     s = sys.argv[1].split(":", 1)
@@ -192,14 +391,14 @@ def main():
         try:
             port = int(s[1])
         except ValueError:
-            print "Error: Erroneous port."
+            log.error("Error: Erroneous port:" + s[1])
             sys.exit(1)
     else:
         port = 6667
     channel = sys.argv[2]
     nickname = sys.argv[3]
 
-    bot = TestBot(channel, nickname, server, port)
+    bot = DownloadBot(channel, nickname, server, port)
     bot.start()
 
 if __name__ == "__main__":
--- lib/calibre/list_award_nominees.py
+++ lib/calibre/list_award_nominees.py
@@ -3,123 +3,25 @@
 import freebase
 
 query = {
-    "id" :   "/en/the_beatles",
-    "type" : "/music/artist",
-    "album" : [{
-        "name" : None,
-        "releases" : [{
-            "release_date" : None,
-            "track" : {
-                "return" : "count"
-            },
-            "sort" : "release_date",
-        }]
+    "id" :   "/en/nebula_award",
+    "type" : "/award/award",
+    "category" : [{
+      "name" : None,
+      "nominees" : [{
+        "award_nominee" : [{
+          "name" : None,
+        }],
+        "nominated_for" : [{
+          "name" : None,
+        }],
+        "limit" : 5000,
+      }]
     }]
 }
 
 results = freebase.mqlread(query)
-for album in results.album:
-    print "Album \"" + album.name + "\" has " + str(album.releases[0].track) + " tracks."
 
-
-# Album "Please Please Me" has 14 tracks.
-# Album "From Me to You" has 2 tracks.
-# Album "Introducing... The Beatles" has 24 tracks.
-# Album "She Loves You" has 2 tracks.
-# Album "With the Beatles" has 14 tracks.
-# Album "I Want to Hold Your Hand" has 2 tracks.
-# Album "Meet the Beatles" has 12 tracks.
-# Album "Second Album" has 11 tracks.
-# Album "Something New" has 11 tracks.
-# Album "A Hard Day's Night" has 36 tracks.
-# Album "I Feel Fine" has 2 tracks.
-# Album "Beatles for Sale" has 14 tracks.
-# Album "Beatles '65" has 11 tracks.
-# Album "The Early Beatles" has 11 tracks.
-# Album "Beatles VI" has 11 tracks.
-# Album "Help! / I'm Down" has 2 tracks.
-# Album "Help!" has 14 tracks.
-# Album "Rubber Soul" has 14 tracks.
-# Album "We Can Work It Out / Day Tripper" has 2 tracks.
-# Album "Paperback Writer / Rain" has 2 tracks.
-# Album "Yesterday... and Today" has 11 tracks.
-# Album "Revolver" has 14 tracks.
-# Album "Strawberry Fields Forever / Penny Lane" has 2 tracks.
-# Album "Sgt. Pepper's Lonely Hearts Club Band" has 24 tracks.
-# Album "Magical Mystery Tour" has 11 tracks.
-# Album "Hello, Goodbye" has 2 tracks.
-# Album "Magical Mystery Tour" has 6 tracks.
-# Album "Lady Madonna" has 2 tracks.
-# Album "Hey Jude" has 2 tracks.
-# Album "The White Album" has 81 tracks.
-# Album "Yellow Submarine" has 13 tracks.
-# Album "Get Back" has 2 tracks.
-# Album "Abbey Road" has 17 tracks.
-# Album "Hey Jude" has 10 tracks.
-# Album "Let It Be" has 12 tracks.
-# Album "1962-1966" has 26 tracks.
-# Album "1967-1970" has 28 tracks.
-# Album "Love Songs" has 25 tracks.
-# Album "The Beatles at the Hollywood Bowl" has 13 tracks.
-# Album "Sgt. Pepper's Lonely Hearts Club Band" has 3 tracks.
-# Album "Past Masters, Volume One" has 18 tracks.
-# Album "Past Masters, Volume Two" has 15 tracks.
-# Album "Rockin' at the Star-Club" has 16 tracks.
-# Album "The Early Tapes of the Beatles" has 14 tracks.
-# Album "Live at the BBC" has 69 tracks.
-# Album "Anthology 1" has 60 tracks.
-# Album "Free as a Bird" has 4 tracks.
-# Album "The Best Of [26 Unforgetable Hit Songs]" has 26 tracks.
-# Album "Real Love" has 4 tracks.
-# Album "Anthology 2" has 45 tracks.
-# Album "Anthology 3" has 50 tracks.
-# Album "Yellow Submarine Songtrack" has 15 tracks.
-# Album "1" has 27 tracks.
-# Album "Let It Be... Naked" has 12 tracks.
-# Album "The Capitol Albums, Volume 1" has 90 tracks.
-# Album "The Capitol Albums, Volume 2" has 68 tracks.
-# Album "16 Superhits, Volume 1" has 16 tracks.
-# Album "16 Superhits, Volume 2" has 16 tracks.
-# Album "16 Superhits, Volume 3" has 16 tracks.
-# Album "16 Superhits, Volume 4" has 16 tracks.
-# Album "1962 Live at Star Club in Hamburg" has 24 tracks.
-# Album "1962 Live Recordings" has 30 tracks.
-# Album "1962-1966 (Red Album)" has 26 tracks.
-# Album "1962-1970" has 18 tracks.
-# Album "A Collection of Beatles Oldies (UK Mono LP)" has 16 tracks.
-# Album "Alternate Rubber Soul" has 28 tracks.
-# Album "Anthology (disc 3)" has 22 tracks.
-# Album "Beatles Tapes III: The 1964 World Tour" has 19 tracks.
-# Album "Beatles VI (Stereo and Mono)" has 22 tracks.
-# Album "Best Selection 1962-1968 Part 3" has 20 tracks.
-# Album "Best, Volume 4: 1964" has 12 tracks.
-# Album "Best, Volume 9: 1966" has 12 tracks.
-# Album "Christmas" has 20 tracks.
-# Album "Complete Rooftop Concert 1" has 22 tracks.
-# Album "EP Collection (disc 1)" has 4 tracks.
-# Album "EP Collection (disc 10)" has 4 tracks.
-# Album "EP Collection (disc 11: Yesterday)" has 4 tracks.
-# Album "EP Collection (disc 12)" has 4 tracks.
-# Album "EP Collection" has 12 tracks.
-# Album "EP Collection (disc 2: Twist and Shout)" has 4 tracks.
-# Album "EP Collection (disc 3)" has 4 tracks.
-# Album "EP Collection (disc 4)" has 4 tracks.
-# Album "EP Collection (disc 5)" has 4 tracks.
-# Album "EP Collection (disc 6)" has 4 tracks.
-# Album "EP Collection (disc 7)" has 4 tracks.
-# Album "EP Collection (disc 8)" has 4 tracks.
-# Album "EP Collection (disc 9)" has 4 tracks.
-# Album "EP Collection, Volume 1" has 32 tracks.
-# Album "EP Collection, Volume 2" has 26 tracks.
-# Album "From Yesterday Forever" has 22 tracks.
-# Album "Get Back" has 20 tracks.
-# Album "Golden Best 20, Volume 1" has 19 tracks.
-# Album "In the Beginning" has 12 tracks.
-# Album "Introducing the Beatles (Us Mono Ver. 2)" has 12 tracks.
-# Album "Live at Star Club 1962, Volume 1" has 11 tracks.
-# Album "Live in Adelaide & Houston, Texas" has 18 tracks.
-# Album "Live in Japan 1964" has 22 tracks.
-# Album "Live in Paris 1965 2" has 24 tracks.
-# Album "Live in Paris, 1965" has 23 tracks.
-# Album "Mythology 2 (disc 1)" has 21 tracks.
-
+for category in results.category:
+    print "Category: " + category.name
+    for nominee in category.nominees:
+        print " %s - %s" % (nominee.award_nominee[0].name, nominee.nominated_for[0].name)
