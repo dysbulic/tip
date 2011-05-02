b4e45311ce66e50fce4774cbfcda9c07adac16e7
Will Holcomb <will@dhappy.org>
2011-05-02 15:34:34 -0400

    checkpoint

--- org/Johns Hopkins/school/medicine/project/Student Outcome Data Research Warehouse/class/≥/2013/report/metrics/m$sql.sql
+++ org/Johns Hopkins/school/medicine/project/Student Outcome Data Research Warehouse/class/≥/2013/report/metrics/m$sql.sql
@@ -5,20 +5,27 @@
 --					FROM [dbo].[rpt_competency_ge2013] c
 --						INNER JOIN [dbo].[rpt_competency_ge2013_score] s ON c.id = s.competency_id
 
-;WITH nodes ( id, child_id, depth, path ) AS
+DECLARE @nodes TABLE( id INT, desc_id INT, depth INT, path VARCHAR(MAX) )
+
+;WITH nodes ( id, desc_id, depth, path ) AS
 ( SELECT tree.node_id, tree.child_id, 1, '|' + CAST(tree.node_id AS VARCHAR(MAX)) + '|'
     FROM rpt_competency_ge2013_tree_map tree
     WHERE tree.node_id IN ( SELECT DISTINCT id FROM rpt_competency_ge2013 )
   UNION ALL
-  SELECT supertree.id, tree.child_id, depth + 1, supertree.path  + '|' + CAST(tree.node_id AS VARCHAR(MAX)) + '|'
+  SELECT supertree.id, tree.child_id, depth + 1, supertree.path + '|' + CAST(tree.node_id AS VARCHAR(MAX)) + '|'
     FROM rpt_competency_ge2013_tree_map tree
     INNER JOIN nodes supertree -- recursive join
-      ON supertree.child_id = tree.node_id
+      ON supertree.desc_id = tree.node_id
    WHERE supertree.path NOT LIKE '%|' + CAST(tree.node_id AS VARCHAR(MAX)) + '|%' -- Avoid cycles
 )
-SELECT * -- child_id, AVG( score ) AS AVG
---FROM Leaves
-FROM rpt_competency_ge2013_tree_map tree
-WHERE node_id NOT IN ( SELECT child_id FROM nodes )
---INNER JOIN rpt_competency_ge2013_score score ON Leaves.child_id = score.competency_id
---GROUP BY Leaves.id, child_id
+INSERT INTO @nodes( id, desc_id, depth, path ) SELECT * FROM nodes
+
+INSERT INTO @nodes( id, desc_id, depth, path )
+  SELECT desc_id, desc_id, depth, nodes.path + '|'
+    FROM @nodes as nodes
+   WHERE desc_id NOT IN ( SELECT id FROM @nodes )
+
+SELECT node.id, AVG( score ) AS AVG
+FROM @nodes node
+INNER JOIN rpt_competency_ge2013_score score ON node.desc_id = score.competency_id
+GROUP BY node.id
