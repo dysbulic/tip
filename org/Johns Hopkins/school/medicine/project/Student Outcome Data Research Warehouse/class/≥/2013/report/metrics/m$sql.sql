ab51d4e0f2ceb3315d9dbe1e2b2f8e705155b754
Will Holcomb <will@dhappy.org>
2011-05-02 14:11:36 -0400

    checkpoint

--- org/Johns Hopkins/school/medicine/project/Student Outcome Data Research Warehouse/class/≥/2013/report/metrics/m$sql.sql
+++ org/Johns Hopkins/school/medicine/project/Student Outcome Data Research Warehouse/class/≥/2013/report/metrics/m$sql.sql
@@ -1,15 +1,3 @@
-DECLARE @startIds TABLE ( id INT PRIMARY KEY )
-
---INSERT INTO @startIds SELECT TOP 1 id FROM #ObjectRelations
---;WITH 
---        Ids (Id) AS ( SELECT competency_id FROM rpt_competency_ge2013_tree_map ),
---   childIds (Id) AS ( SELECT child_id      FROM rpt_competency_ge2013_tree_map )
-INSERT INTO @startIds
---  SELECT DISTINCT Ids.Id FROM Ids
---    LEFT JOIN childIds on Ids.Id = childIds.Id
---    WHERE childIds.Id IS NULL
-  SELECT DISTINCT id FROM rpt_competency_ge2013
-
 --		SELECT competency_id, class_of_desc,
 --		       AVG(score) AS avg, STDEV(score) as stddev, COUNT(score) as count
 --
@@ -20,13 +8,13 @@ INSERT INTO @startIds
 ;WITH Leaves ( id, child_id, depth, path ) AS
 ( SELECT tree.competency_id, tree.child_id, 1, '|' + CAST(tree.competency_id AS VARCHAR(MAX)) + '|'
     FROM rpt_competency_ge2013_tree_map tree
-    WHERE tree.competency_id IN ( SELECT id FROM @startIds )
+    WHERE tree.competency_id IN ( SELECT DISTINCT id FROM rpt_competency_ge2013 )
   UNION ALL
   SELECT containing_tree.id, tree.child_id, depth + 1, containing_tree.path  + '|' + CAST(tree.competency_id AS VARCHAR(MAX)) + '|'
     FROM rpt_competency_ge2013_tree_map tree
     INNER JOIN Leaves containing_tree -- recursive join
       ON containing_tree.child_id = tree.competency_id
-   WHERE containing_tree.path NOT LIKE '%|' + CAST(tree.competency_id AS VARCHAR(MAX)) + '|%' -- Avoid cycles
+   WHERE containing_tree.path NOT LIKE '%|' + CAST(tree.competency_id AS VARCHAR(MAX)) + '||' + CAST(tree.competency_id AS VARCHAR(MAX)) + '|%' -- Avoid cycles
 )
 SELECT Leaves.id, child_id, depth -- AVG( score )
 FROM  Leaves
