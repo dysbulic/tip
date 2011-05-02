9c09940b2d7245315a4a86c250c71584613afad9
Will Holcomb <will@dhappy.org>
2011-05-02 10:55:15 -0400

    checkpoint

--- site/stackoverflow/user/emzero/Multiple parents tree (or digraph) implementation sql server 2005/procedure/m$sql.sql
+++ site/stackoverflow/user/emzero/Multiple parents tree (or digraph) implementation sql server 2005/procedure/m$sql.sql
@@ -1,8 +1,4 @@
-create table #ObjectRelations
-(
-    Id varchar(20),
-    NextId varchar(20)
-)
+CREATE TABLE IF NOT EXISTS #ObjectRelations ( Id varchar(20), NextId varchar(20) )
 
 /* Cycle */
 /*
@@ -12,7 +8,6 @@ insert into #ObjectRelations values ('C', 'A')
 */
 
 /* Multi root */
-
 insert into #ObjectRelations values ('G', 'B')
 insert into #ObjectRelations values ('A', 'B') 
 insert into #ObjectRelations values ('B', 'C')
@@ -22,62 +17,34 @@ insert into #ObjectRelations values ('C', 'D')
 insert into #ObjectRelations values ('E', 'F') 
 insert into #ObjectRelations values ('D', 'F') 
 
-
-declare @startIds table
-(
-    Id varchar(20) primary key
-)
+declare @startIds table ( Id varchar(20) primary key )
 
 ;WITH 
-    Ids (Id) AS
-    (
-        SELECT  Id
-        FROM    #ObjectRelations
-    ),
-    NextIds (Id) AS
-    (
-        SELECT  NextId
-        FROM    #ObjectRelations
-    )
+    Ids (Id) AS ( SELECT Id FROM #ObjectRelations ),
+    NextIds (Id) AS ( SELECT NextId FROM #ObjectRelations )
 INSERT INTO @startIds
-/* This select will not return anything since there are not objects without predecessor, because it's a cyclic of course */
-SELECT DISTINCT
-    Ids.Id
-FROM
-    Ids
-LEFT JOIN
-    NextIds on Ids.Id = NextIds.Id
-WHERE
-    NextIds.Id IS NULL
-UNION
-/* So let's just pick anyone. (the way I will be getting the starting object for a cyclic doesn't matter for the regarding problem)*/
-SELECT TOP 1 Id FROM Ids
+  /* This select will not return anything since there are not objects without predecessor, because it's a cyclic of course */
+  SELECT DISTINCT Ids.Id FROM Ids
+    LEFT JOIN NextIds on Ids.Id = NextIds.Id
+    WHERE NextIds.Id IS NULL
+  UNION
+    /* So let's just pick anyone. (the way I will be getting the starting object for a cyclic doesn't matter for the regarding problem)*/
+    SELECT TOP 1 Id FROM Ids
 
-;WITH Objects (Id, NextId, [Level], Way) AS
+;WITH Objects (Id, NextId, [Level], Path) AS
 ( -- This is the 'Anchor' or starting point of the recursive query
-  SELECT rel.Id,
-         rel.NextId,
-         1,
-         CAST(rel.Id as VARCHAR(MAX))
+  SELECT rel.Id, rel.NextId, 1, CAST(rel.Id as VARCHAR(MAX))
     FROM #ObjectRelations rel
-   WHERE rel.Id IN (SELECT Id FROM @startIds)
-
-   UNION ALL -- This is the recursive portion of the query
-
-  SELECT rel.Id,
-         rel.NextId,
-         [Level] + 1,
-         RecObjects.Way + ', ' + rel.Id
+    WHERE rel.Id IN ( SELECT Id FROM @startIds )
+  UNION ALL
+  SELECT rel.Id, rel.NextId, [Level] + 1, RecObjects.Path + ', ' + rel.Id
     FROM #ObjectRelations rel
-   INNER JOIN Objects RecObjects -- Note the reference to CTE table name (Recursive Join)
+   INNER JOIN Objects RecObjects -- recursive join
       ON rel.Id = RecObjects.NextId
-   WHERE RecObjects.Way NOT LIKE '%' + rel.Id + '%'
+   WHERE RecObjects.Path NOT LIKE '%' + rel.Id + '%'
 
 )
-SELECT  DISTINCT 
-        Id,
-        NextId,
-        [Level]
+SELECT DISTINCT Id, NextId, [Level], Path
 FROM    Objects
 ORDER BY [Level]
 
