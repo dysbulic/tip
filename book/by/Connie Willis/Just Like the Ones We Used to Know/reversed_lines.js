611acf610650ce35b2b277fc05594d8789c168fe
sonja <sonja@ubuntu.(none)>
2011-12-19 17:54:46 -0500

    revamping w/ interlaced clip paths

--- book/by/Connie Willis/Just Like the Ones We Used to Know/reversed_lines.js
+++ book/by/Connie Willis/Just Like the Ones We Used to Know/reversed_lines.js
@@ -1,4 +1,8 @@
 $( function() {
+       var maxWidth = 500
+       
+
+       var $body = $("body")
        var $svg = $( document.createElementNS( "http://www.w3.org/2000/svg", "svg" ) )
        $svg.attr( {
 		     width : "100%",
@@ -10,14 +14,61 @@ $( function() {
 		 } )
        $bg = $( document.createElementNS( "http://www.w3.org/2000/svg", "rect" ) )
        $bg.attr( {
-		     width : 500,
-		     height : 500,
+		     x : 0,
+		     y : 0,
+		     width : $body.width(),
+		     height : $body.height(),
 		 } )
        $bg.css( {
-		    fill: "red",
+		    fill: "lightblue",
 		} )
+       $bg.get(0).style.fill = "lightblue"
        $svg.append( $bg )
-       var $body = $("body")
+
        $body.prepend( $svg )
-       console.log( $body )
-} )
+
+       var $lines = []
+
+       $('p').slice( 0, 9 ).each(
+	   function walkPara() {
+	       console.log( this )
+
+	       var text = this.textContent.split(" ")
+	       var idx = 0
+	       
+	       var $line
+	       var $last
+	       
+	       while( idx < text.length ) {
+		   if( ! $line ) {
+		       $line = $( document.createElementNS( "http://www.w3.org/2000/svg", "text" ) )
+		       $svg.append( $line )
+		       if( $lines.length % 2 == 0 ) {
+			   $line.attr( {
+					   x : -530,
+					   y : 17 * $lines.length + 20,
+					   transform : "scale(-1, 1)",
+				       } )
+		       } else {
+			   $line.attr( {
+					   x : 20,
+					   y : 17 * $lines.length + 20,
+				       } )
+		       }
+		       $lines.push( $line )
+		   }
+		   var orig = $line.text()
+		   if( orig.length > 0 ) {
+		       orig += " "
+		   }
+		   $line.text( orig + text[idx++] )
+		   
+		   if( orig.length > 0 &&
+		       $line.get(0).getBBox().width > maxWidth ) {
+			   $line.text( orig )
+			   $line = undefined
+			   --idx
+		       }
+	       }			
+	   } )
+   } )
