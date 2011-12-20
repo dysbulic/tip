78bd0d661d469e1fcf9e55c053e23a457792c8a8
sonja <sonja@ubuntu.(none)>
2011-12-20 15:49:16 -0500

    switching to interlace

--- book/by/Connie Willis/Just Like the Ones We Used to Know/reversed_lines.js
+++ book/by/Connie Willis/Just Like the Ones We Used to Know/reversed_lines.js
@@ -1,74 +1,23 @@
 $( function() {
-       var maxWidth = 500
-       
+       var $body = $('body')
 
-       var $body = $("body")
-       var $svg = $( document.createElementNS( "http://www.w3.org/2000/svg", "svg" ) )
+       var $svg = $( document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ) )
        $svg.attr( {
-		     width : "100%",
-		     height : "100%",
-		     viewBox : "0 0 500 500",
-		 } )
-       $svg.css( {
-		     position : "fixed"
-		 } )
-       $bg = $( document.createElementNS( "http://www.w3.org/2000/svg", "rect" ) )
-       $bg.attr( {
-		     x : 0,
-		     y : 0,
 		     width : $body.width(),
 		     height : $body.height(),
+		     viewBox : "0 0 " + $body.width() + " " + $body.height(),
 		 } )
-       $bg.css( {
-		    fill: "lightblue",
-		} )
-       $bg.get(0).style.fill = "lightblue"
-       $svg.append( $bg )
 
-       $body.prepend( $svg )
+       var $forward = $( document.createElementNS( 'http://www.w3.org/2000/svg', 'foreignObject' ) )
+       $forward.attr( {
+			  x : 0, y : 0,
+			  width : $body.width(), height : $body.height(),
+			  viewBox : "0 0 " + $body.width() + " " + $body.height(),
+		 } )
+       $svg.append( $forward )
 
-       var $lines = []
-
-       $('p').slice( 0, 9 ).each(
-	   function walkPara() {
-	       console.log( this )
-
-	       var text = this.textContent.split(" ")
-	       var idx = 0
-	       
-	       var $line
-	       var $last
-	       
-	       while( idx < text.length ) {
-		   if( ! $line ) {
-		       $line = $( document.createElementNS( "http://www.w3.org/2000/svg", "text" ) )
-		       $svg.append( $line )
-		       if( $lines.length % 2 == 0 ) {
-			   $line.attr( {
-					   x : -530,
-					   y : 17 * $lines.length + 20,
-					   transform : "scale(-1, 1)",
-				       } )
-		       } else {
-			   $line.attr( {
-					   x : 20,
-					   y : 17 * $lines.length + 20,
-				       } )
-		       }
-		       $lines.push( $line )
-		   }
-		   var orig = $line.text()
-		   if( orig.length > 0 ) {
-		       orig += " "
-		   }
-		   $line.text( orig + text[idx++] )
-		   
-		   if( orig.length > 0 &&
-		       $line.get(0).getBBox().width > maxWidth ) {
-			   $line.text( orig )
-			   $line = undefined
-			   --idx
-		       }
-	       }			
-	   } )
+       var $html = $( '<html/>' ).append( $('head') )
+       $forward.append( $html )
+       
+       $('html').append( $('<body/>').append( $svg ) )
    } )
