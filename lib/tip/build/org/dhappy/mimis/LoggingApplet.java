76c9b08deee1a92331ee84857c74874ca896e169
sonja <sonja@ubuntu.(none)>
2012-01-08 14:58:36 -0500

    logs in chrome

---
+++ lib/tip/build/org/dhappy/mimis/LoggingApplet.java
@@ -0,0 +1,31 @@
+package org.dhappy.mimis;
+
+import java.util.logging.Logger;
+
+import javax.swing.JApplet;
+
+public class LoggingApplet extends JApplet {
+    private static Logger log =
+	Logger.getLogger( LoggingApplet.class.getName() );
+
+    public void init() {
+	log.info( "Initialized: " + LoggingApplet.class.getName() );
+    }
+
+    public void start() {
+	System.out.println( "Start: " + LoggingApplet.class.getName() );
+	log.info( "Start: " + LoggingApplet.class.getName() );
+    }
+
+    public void stop() {
+	log.info( "Stop: " + LoggingApplet.class.getName() );
+    }
+
+    public void destroy() {
+	log.info( "Destroying: " + LoggingApplet.class.getName() );
+    }
+
+    public static void main( String[] args ) {
+	log.info( "Instantiated: " + LoggingApplet.class.getName() );
+    }
+}
