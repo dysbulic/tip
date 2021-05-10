6b638b952313de593013cf6a6fa74198bc909f73
Will Holcomb <dys@dhappy.org>
2021-05-10 17:53:12 -0400

    importin' from old old site & movin' programs to subdir 🦊

---
+++ lib/Tip/example/WWIII/index.php
@@ -0,0 +1,26 @@
+<?xml version="1.0" standalone="yes"?>
+<?php
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:lxslt="http://xml.apache.org/xslt" xmlns:doc="http://docbook.org/ns/docbook">
+  <head>
+    <title>Tip</title>
+    <style type="text/css">
+      html, body { height: 100%; padding: 0; margin: 0; }
+    </style>
+    <link rel="stylesheet" href="tip.css" type="text/css"/>
+  </head>
+  <body>
+
+    <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+      try {
+        var pageTracker = _gat._getTracker("UA-2592249-7");
+        pageTracker._trackPageview();
+      } catch(err) {}
+    </script>
+  </body>
+</html>
---
+++ lib/Tipspace/src/org/dhappy/tip/TipspaceServlet.java
@@ -0,0 +1,13 @@
+package org.dhappy.tip;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TipspaceServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
