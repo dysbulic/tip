3c9fa3a12779a597673c88d618619e111cc6f523
dysbulic <dys@dhappy.org>
2023-09-08 16:16:24 -0400

    workin' on Durban Poison 🎹

---
+++ program/piclinks/piclinks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { Glob } from 'glob'
+import fs from 'fs'
+
+const outdir = 'svglinks'
+await fs.promises.mkdir(outdir, { recursive: true })
+
+const iter = new Glob('**/*.svg', { ignore: ['**/node_modules/**', '**/build/**', '**/dist/**'] })
+for await (const file of iter) {
+  let to = file.replace(/(\/)?(svg)?\.svg$/, '')
+  to = to.replace(/\//g, '⁄')
+  to = `${outdir}/${to}`
+  const from = `${outdir.replace(/[^/]+/g, '..')}/${file}`
+  try {
+    fs.access(to, fs.constants.F_OK)
+    console.debug(`Exists: ${to}`)
+  } catch {
+    console.debug(`Link: ${from} → ${to}`)
+    await fs.promises.symlink(from, to)
+  }
+}
